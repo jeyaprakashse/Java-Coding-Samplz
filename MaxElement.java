bddf4ad4b2c9f1bb574057a86b9e35a8a5c04a0d
Jeya Prakash <jeyaprakashse@gmail.com>
2024-09-29 22:37:54 +0530

    First Program

---
+++ MaxElement.java
@@ -0,0 +1,16 @@
+public class MaxElement
+
+{
+    public static void main(String[] args) {
+        int array[]={1,4,3,5,0,8};
+        int max =array[0];
+        for (int i=1;i<array.length;i++)
+        {
+           if( array[i] > max)
+           {
+            max=array[i];
+           }
+        }
+        System.out.println("Maximum Element :"+max);
+    }
+}
