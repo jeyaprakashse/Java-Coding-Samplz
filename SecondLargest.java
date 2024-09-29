c8aeaa44c75a4d51d92f17f00aa97afd1e64275b
Jeya Prakash <jeyaprakashse@gmail.com>
2024-09-30 00:35:47 +0530

    SecondLargest

--- SecondLargest.java
+++ SecondLargest.java
@@ -3,22 +3,18 @@ public class SecondLargest {
         int array[]={1,3,4,5,2,9,19,7,10};
         int largest=array[0];
         int SecondLargest=array[8];
-
         for(int num:array)
         {
             if(num>largest)
             {
                 SecondLargest=largest;
                 largest=num;
-            
             }
             else if(num>SecondLargest && num!=largest)
-
                 {
                     SecondLargest=num;
             }
         }
         System.out.println("Largest Element:"+largest+"\n"+"Second Largest:"+SecondLargest);
-
     }
 }
