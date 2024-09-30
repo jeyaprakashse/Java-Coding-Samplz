d7029b1f534bd461941616acf81c9017c7aa1c60
Jeya Prakash <jeyaprakashse@gmail.com>
2024-09-30 22:48:24 +0530

    Substring with its value

--- Substring.java
+++ Substring.java
@@ -1,20 +1,24 @@
-        import java.util.HashSet;
+import java.util.HashSet;
 public class Substring {
     public static void main(String[] args) {
         String s = "abcdefghikjmabcdefghikjncbb"; 
         int maxLength = 0;
         int left = 0;
+        String longstring ="";
         HashSet<Character> set = new HashSet<>();
-
         for (int right = 0; right < s.length(); right++) {
             while (set.contains(s.charAt(right))) {
                 set.remove(s.charAt(left));
                 left++;
             }
             set.add(s.charAt(right));
-            maxLength = Math.max(maxLength, right - left + 1);
+            if(right-left+1>maxLength)
+            {
+                maxLength=right-left+1;
+                longstring =s.substring(left,right+1);
+            }
         }
-
+        System.out.println("The longest substring: " + longstring);
         System.out.println("Length of the longest substring: " + maxLength);
     }
 }
